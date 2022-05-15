6fd3b58337382c4e4fdc138085034b103a71dba8
popsoft01 <popoolatunde52@gmail.com>
2022-05-15 18:52:40 +0100

    app user update

--- src/main/java/com/example/springsecurityandjwttoken/SpringSecurityAndJwtTokenApplication.java
+++ src/main/java/com/example/springsecurityandjwttoken/SpringSecurityAndJwtTokenApplication.java
@@ -8,50 +8,55 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
 
 @SpringBootApplication
-public class SpringSecurityAndJwtTokenApplication implements CommandLineRunner {
+public class SpringSecurityAndJwtTokenApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityAndJwtTokenApplication.class, args);
     }
-    @Autowired
-    AppUserService userService;
+//    @Autowired
+//    AppUserService userService;
 
-//    CommandLineRunner run(){
-//        return args -> {
-//            userService.SaveRole(new Role(null, "ROLE_USER"));
-//            userService.SaveRole(new Role(null, "ROLE_MANAGER"));
-//            userService.SaveRole(new Role(null, "ROLE_ADMIN"));
-//            userService.SaveRole(new Role(null, "ROLE_SUPER_ADMIN"));
-//
-//            userService.saveUser(new AppUser(null, "love okele", "love", "1234", new ArrayList<>()));
-//            userService.saveUser(new AppUser(null, "lov akele", "lov", "1234", new ArrayList<>()));
-//            userService.saveUser(new AppUser(null, "love ele", "ele", "1234", new ArrayList<>()));
-//            userService.saveUser(new AppUser(null, "love omole", "omole", "1234", new ArrayList<>()));
-//
-//            userService.addRoleToUser("love","ROLE_USER");
-//            userService.addRoleToUser("love","ROLE_ADMIN");
-//            userService.addRoleToUser("ele","ROLE_USER");
-//        };
+    @Bean
+    CommandLineRunner run(AppUserService userService){
+        return args -> {
+            userService.SaveRole(new Role(null, "ROLE_USER"));
+            userService.SaveRole(new Role(null, "ROLE_MANAGER"));
+            userService.SaveRole(new Role(null, "ROLE_ADMIN"));
+            userService.SaveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+            userService.saveUser(new AppUser(null, "love okele", "love", "1234", new ArrayList<>()));
+            userService.saveUser(new AppUser(null, "lov akele", "lov", "1234", new ArrayList<>()));
+            userService.saveUser(new AppUser(null, "love ele", "ele", "1234", new ArrayList<>()));
+            userService.saveUser(new AppUser(null, "love omole", "omole", "1234", new ArrayList<>()));
+            userService.saveUser(new AppUser(null, "Ojo omole", "Ojo", "1234", new ArrayList<>()));
+
+            userService.addRoleToUser("love","ROLE_USER");
+            userService.addRoleToUser("love","ROLE_ADMIN");
+            userService.addRoleToUser("ele","ROLE_USER");
+            userService.addRoleToUser("Ojo","ROLE_MANAGER");
+        };
 //    }
 
-    @Override
-    public void run(String... args) throws Exception {
-        userService.SaveRole(new Role(null, "ROLE_USER"));
-        userService.SaveRole(new Role(null, "ROLE_MANAGER"));
-        userService.SaveRole(new Role(null, "ROLE_ADMIN"));
-        userService.SaveRole(new Role(null, "ROLE_SUPER_ADMIN"));
-
-        userService.saveUser(new AppUser(null, "love okele", "love", "1234", new ArrayList<>()));
-        userService.saveUser(new AppUser(null, "lov akele", "lov", "1234", new ArrayList<>()));
-        userService.saveUser(new AppUser(null, "love ele", "ele", "1234", new ArrayList<>()));
-        userService.saveUser(new AppUser(null, "love omole", "omole", "1234", new ArrayList<>()));
-
-        userService.addRoleToUser("love","ROLE_USER");
-        userService.addRoleToUser("love","ROLE_ADMIN");
-        userService.addRoleToUser("ele","ROLE_USER");
+//    @Override
+//    public void run(String... args) throws Exception {
+//        userService.SaveRole(new Role(null, "ROLE_USER"));
+//        userService.SaveRole(new Role(null, "ROLE_MANAGER"));
+//        userService.SaveRole(new Role(null, "ROLE_ADMIN"));
+//        userService.SaveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+//        implements CommandLineRunner
+//
+//        userService.saveUser(new AppUser(null, "love okele", "love", "1234", new ArrayList<>()));
+//        userService.saveUser(new AppUser(null, "lov akele", "lov", "1234", new ArrayList<>()));
+//        userService.saveUser(new AppUser(null, "love ele", "ele", "1234", new ArrayList<>()));
+//        userService.saveUser(new AppUser(null, "love omole", "omole", "1234", new ArrayList<>()));
+//
+//        userService.addRoleToUser("love","ROLE_USER");
+//        userService.addRoleToUser("love","ROLE_ADMIN");
+//        userService.addRoleToUser("ele","ROLE_USER");
     }
 }
--- src/main/java/com/example/springsecurityandjwttoken/service/UserServiceImpl.java
+++ src/main/java/com/example/springsecurityandjwttoken/service/UserServiceImpl.java
@@ -6,15 +6,38 @@ import com.example.springsecurityandjwttoken.domain.AppUser;
 import com.example.springsecurityandjwttoken.domain.Role;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service @RequiredArgsConstructor @Transactional @Slf4j
-public class UserServiceImpl implements AppUserService{
+public class UserServiceImpl implements AppUserService, UserDetailsService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser user = userRepository.findByUsername(username);
+        if (user == null){
+            log.error("User not found in the database");
+            throw new UsernameNotFoundException("User not found in the database");
+        }else{
+            log.info("User found in the database: {}", username);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new User(user.getUsername(),user.getPassword(), authorities);
+    }
 
     @Override
     public AppUser saveUser(AppUser user) {
@@ -48,4 +71,6 @@ public class UserServiceImpl implements AppUserService{
         log.info("fetching user to the database");
         return userRepository.findAll();
     }
+
+
 }
